6e9f61b73cf6a40a63fd07e1d9ec41c9ada334ab
peyman <peyman.alad@gmail.com>
2025-11-12 15:57:23 +0330

    WIP

--- Dario.Service.Card.API/obj/Debug/net9.0/Dario.Service.Card.API.AssemblyInfo.cs
+++ Dario.Service.Card.API/obj/Debug/net9.0/Dario.Service.Card.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dario.Service.Card.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a083827c726cac08ba6b8006e089431acb9cbf6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c66be7a9289757a1463cab1abbff4be127b16ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dario.Service.Card.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dario.Service.Card.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
