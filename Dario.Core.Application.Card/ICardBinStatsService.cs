3270edb3b1b5b01e98fe6149dab4176bdd9af89e
Peyman <peyman.alad@gmail.com>
2025-11-26 12:48:44 +0330

    WIP

---
+++ Dario.Core.Application.Card/ICardBinStatsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dario.Core.Application.Card
+{
+    public interface ICardBinStatsService
+    {
+        Task IncrementAsync(string bin, CancellationToken cancellationToken = default);
+    }
+
+}
---
+++ Dario.Core.Application.Card/OracleCardBinStatsService.cs
@@ -0,0 +1,39 @@
+﻿using Dario.Core.Abstraction.Card.Options;
+using Dario.Core.Application.Card;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+using System.Data;
+
+public class OracleCardBinStatsService : ICardBinStatsService
+{
+    private readonly string _connectionString;
+    private readonly IOptions<CardServicesOptions> _configuration;
+
+    public OracleCardBinStatsService(IOptions<CardServicesOptions> configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task IncrementAsync(string bin, CancellationToken cancellationToken = default)
+    {
+        if (string.IsNullOrWhiteSpace(bin))
+            return;
+        using var connection = CreateConnection();
+        await connection.OpenAsync();
+
+        using var cmd = connection.CreateCommand();
+        cmd.CommandText = "IncrementCardBinDailyStat";
+        cmd.CommandType = CommandType.StoredProcedure;
+
+        cmd.Parameters.Add("p_Bin", OracleDbType.Varchar2, 6).Value = bin;
+
+        await cmd.ExecuteNonQueryAsync(cancellationToken);
+    }
+    private OracleConnection CreateConnection()
+    => new OracleConnection(_configuration.Value.ConnectionString);
+
+    private OracleConnection CreateQueryConnection()
+        => new OracleConnection(_configuration.Value.ConnectionStringQuery);
+}
