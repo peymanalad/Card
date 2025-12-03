eeb2fcce5e6e6949b9d120a52bde572c04684ae1
peyman <peyman.alad@gmail.com>
2025-12-03 21:23:03 +0330

    WIP

---
+++ Dario.Core.Domain.Card/CardBinStatsDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dario.Core.Domain.Card
+{
+    public class CardBinStatsDto
+    {
+        public string Bin { get; set; } = default!;
+        public string BankName { get; set; } = default!;
+        public long TodayCount { get; set; }
+        public long MonthCount { get; set; }
+        public long TotalCount { get; set; }
+
+        public string LogoUrl { get; set; } = default!;
+    }
+
+}
